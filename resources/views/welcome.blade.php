898de8008e019643febe96185037daabb456e97f
hanmiton <hanmilton_12@outlook.com>
2017-12-21 21:41:21 -0500

    cambias como acceder a prpopiedades de objetos  ocn flecha

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
 <div class="row">
     @forelse($messages as $message)
         <div class="col-6">
-            <img class="img-thumbnail" src="{{$message['image']}}">
+            <img class="img-thumbnail" src="{{ $message->image }}">
             <p class="card-text">
-                {{ $message['content'] }}
-                <a href="/messages/{{ $message['id'] }}"> Leer m as</a>
+                {{ $message->content }}
+                <a href="/messages/{{ $message->id }}"> Leer m as</a>
             </p>
         </div>
     @empty
