3ac0e933a07a91e8cca877468ccc1f71fa108be8
hanmiton <hanmilton_12@outlook.com>
2017-12-23 11:30:54 -0500

    has-danger

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <form action="/messages/create" method="post">
         {{ csrf_field() }}
-        <div class="form-group @if($errors->has('message')) danger @endif">
+        <div class="form-group @if($errors->has('message')) has-danger @endif">
             <input type="text" name="message" class="form-control" placeholder="Que estas pensando?">
             @if ($errors->has('message'))
                 @foreach ($errors->get('message') as $error)
