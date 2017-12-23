b58b64a06ed8d64fce6af006380a50130ace1f74
hanmiton <hanmilton_12@outlook.com>
2017-12-23 11:19:16 -0500

    class danger if error on form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,13 @@
 <div class="row">
     <form action="/messages/create" method="post">
         {{ csrf_field() }}
-        <div class="form-group">
+        <div class="form-group" @if($errors->has('message')) has-danger @endif">
             <input type="text" name="message" class="form-control" placeholder="Que estas pensando?">
+            @if ($errors->has('message'))
+                @foreach ($errors->get('message') as $error)
+                    <div class= "form-control-feedback"> {{ $error }} </div>
+                @endforeach
+            @endif
         </div>
     </form>
 </div>
