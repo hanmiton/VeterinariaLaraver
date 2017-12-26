5ea7e0b58594c2ac877f7181f52b4c89e16dba31
hanmiton <hanmilton_12@outlook.com>
2017-12-26 12:35:50 -0500

    solucionando problema de namespace

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Message;
-use App\Http\Request\CreateMessageRequest;
+use App\Http\Requests\CreateMessageRequest;
 use Illuminate\Http\Request;
 
 class MessagesController extends Controller
