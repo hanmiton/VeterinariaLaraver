c6f1525546a222867119dda831f9085bba2f2773
hanmiton <hanmilton_12@outlook.com>
2017-12-27 12:40:40 -0500

    creando 100 datos de prueba con seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory(App\Message::class)->create();
+        factory(App\Message::class)
+        	->times(100)
+        	->create();
     }
 }
