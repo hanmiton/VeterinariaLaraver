3cc24dd9e2ce779a4389ae0575f3bf7f5b6487ff
hanmiton <hanmilton_12@outlook.com>
2017-12-27 12:15:35 -0500

    usando factories  para crear datos de prueba

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,11 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Message::class, function(
+	Faker $faker) {
+	return [
+		'cotent' => $faker->realText(),
+		'image' => $faker->imageUrl(600,338)
+	];
+});
