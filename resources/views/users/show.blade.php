1661e6dcea44e83d804e0a6b08054d1b387ec8d8
hanmiton <hanmilton_12@outlook.com>
2018-01-04 04:05:36 -0500

    añadiendo a seguidores

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 <h1>{{ $user->name }}</h1>
+<a href="/{{ $user->username }}/follows" class="btn btn-link">
+	Sigue a <span class="badge badge-default">{{ $user->follows->count() }}</span>
+</a>
+
+<a href="/{{ $user->username }}/followed" class="btn btn-link">
+	Siguidores <span class="badge badge-default">{{ $user->followers->count() }}</span>
+</a>
+
 
 @if(Auth::check())
 	@if(Auth::user()->isFollowing($user))
