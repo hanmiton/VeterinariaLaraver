57c1505cd5f5c3a28a8a5819fef0eeb149689b20
hanmiton <hanmilton_12@outlook.com>
2018-01-03 23:02:49 -0500

    añadiendo relacion belongs

--- resources/views/messages/message.blade.php
+++ resources/views/messages/message.blade.php
@@ -1,6 +1,6 @@
 <img class="img-thumbnail" src="{{ $message->image }}">
 <p class="card-text">
-	<div class="text-muted">Escrito por : {{ $message->user_id }}</div>
+	<div class="text-muted">Escrito por : {{ $message->user->name }}</div>
     {{ $message->cotent }}
     <a href="/messages/{{ $message->id }}"> Leer m as</a>
 </p>
