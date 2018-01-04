6df43435af2034e76c8a02809cef62ad3df3251f
hanmiton <hanmilton_12@outlook.com>
2018-01-03 22:56:49 -0500

    añdieindo escrito por

--- resources/views/messages/message.blade.php
+++ resources/views/messages/message.blade.php
@@ -1,6 +1,6 @@
 <img class="img-thumbnail" src="{{ $message->image }}">
 <p class="card-text">
-	<span class="text-muted"> {{ $message->user_id }}</span>
+	<div class="text-muted">Escrito por : {{ $message->user_id }}</div>
     {{ $message->cotent }}
     <a href="/messages/{{ $message->id }}"> Leer m as</a>
 </p>
