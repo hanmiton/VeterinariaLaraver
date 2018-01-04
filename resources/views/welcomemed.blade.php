a52b3b4e658eae359ee1220e5d16a783fa2a5bfc
hanmiton <hanmilton_12@outlook.com>
2018-01-04 11:38:52 -0500

    creando vistas medicos

--- resources/views/welcomemed.blade.php
+++ resources/views/welcomemed.blade.php
@@ -1,32 +1,100 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <form action="/medicos/create" method="post">
-        {{ csrf_field() }}
-        <div class="form-group @if($errors->has('medico')) has-danger @endif">
-            <input type="text" name="medico" class="form-control" placeholder="Que estas pensando?">
-            @if ($errors->has('medico'))
-                @foreach ($errors->get('medico') as $error)
-                    <div class= "form-control-feedback"> {{ $error }} </div>
-                @endforeach
-            @endif
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Registro Medico</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="/medicos/create"">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('direccion') ? ' has-error' : '' }}">
+                            <label for="direccion" class="col-md-4 control-label">direccion</label>
+
+                            <div class="col-md-6">
+                                <input id="direccion" type="text" class="form-control" name="direccion" value="{{ old('direccion') }}" required autofocus>
+
+                                @if ($errors->has('direccion'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('direccion') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
+                        <div class="form-group{{ $errors->has('telefono') ? ' has-error' : '' }}">
+                            <label for="telefono" class="col-md-4 control-label">telefono</label>
+
+                            <div class="col-md-6">
+                                <input id="telefono" type="text" class="form-control" name="telefono" value="{{ old('telefono') }}" required autofocus>
+
+                                @if ($errors->has('telefono'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('telefono') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('celular') ? ' has-error' : '' }}">
+                            <label for="celular" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="celular" type="celular" class="form-control" name="celular" value="{{ old('celular') }}" required>
+
+                                @if ($errors->has('celular'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('celular') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('cod_vet') ? ' has-error' : '' }}">
+                            <label for="cod_vet" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="cod_vet" type="cod_vet" class="form-control" name="cod_vet" value="{{ old('cod_vet') }}" required>
+
+                                @if ($errors->has('cod_vet'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('cod_vet') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('cedula') ? ' has-error' : '' }}">
+                            <label for="cedula" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="cedula" type="cedula" class="form-control" name="cedula" value="{{ old('cedula') }}" required>
+
+                                @if ($errors->has('cedula'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('cedula') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-    </form>
-</div>
-<div class="row">
-    @forelse($medicos as $medico)
-        <div class="col-6">
-            @include('medicos.medico')
-        </div>
-    @empty
-        <p>No hay medicos destacados. </p>
-    @endforelse
-  
-    @if(count($medicos))
-    <div class="mt-2 mx-auto">
-        {{ $medicos->links('pagination::bootstrap-4') }}
     </div>
-    @endif
 </div>
 @endsection
