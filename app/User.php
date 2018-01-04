8b3a728ff26d2c2141268b67a57b684f590da0f6
hanmiton <hanmilton_12@outlook.com>
2018-01-04 02:03:31 -0500

    ñadiendo user has many messages

--- app/User.php
+++ app/User.php
@@ -29,6 +29,6 @@ class User extends Authenticatable
 
     public function messages()
     {
-        return $this->hasMany(Message::class);
+        return $this->hasMany(Message::class)->orderBy('created_at', 'desc');
     }
 }
