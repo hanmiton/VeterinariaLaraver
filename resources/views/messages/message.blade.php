eed5db8ea2f70f42ef6d5d98ca40d2a2adf35e60
hanmiton <hanmilton_12@outlook.com>
2018-01-03 23:09:10 -0500

    añadiendo link para user

--- resources/views/messages/message.blade.php
+++ resources/views/messages/message.blade.php
@@ -1,6 +1,6 @@
 <img class="img-thumbnail" src="{{ $message->image }}">
 <p class="card-text">
-	<div class="text-muted">Escrito por : {{ $message->user->name }}</div>
+	<div class="text-muted">Escrito por : <a href="/{{ $message->user->username}}"></a>{{ $message->user->name }}</div>
     {{ $message->cotent }}
     <a href="/messages/{{ $message->id }}"> Leer m as</a>
 </p>
