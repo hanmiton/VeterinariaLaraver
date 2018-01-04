e5daadf844d3b9a0bb3fbd9ec5b10ffc9d553525
hanmiton <hanmilton_12@outlook.com>
2018-01-03 22:53:37 -0500

    añadiendo id en vista message

--- resources/views/messages/message.blade.php
+++ resources/views/messages/message.blade.php
@@ -1,5 +1,6 @@
 <img class="img-thumbnail" src="{{ $message->image }}">
 <p class="card-text">
+	<span class="text-muted"> {{ $message->user_id }}</span>
     {{ $message->cotent }}
     <a href="/messages/{{ $message->id }}"> Leer m as</a>
 </p>
