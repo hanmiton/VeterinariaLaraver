01ab1ec8bdd81a874f1a7ff94dc1033244853bfa
hanmiton <hanmilton_12@outlook.com>
2018-01-05 13:48:09 -0500

    mostrando  los  mensajes por id

--- resources/views/users/conversation.blade.php
+++ resources/views/users/conversation.blade.php
@@ -2,4 +2,14 @@
 
 @section('content')
 <h1>Conversacion con {{ $conversation->users->except($user->id)->implode('name', ', ') }} </h1>
+
+<div class="card">
+	@foreach($conversation->privateMessages as $message)
+	<div class="card-block">
+		<p>{{ $message->user->name }} dijo...</p>
+		<p>{{ $message->message }}</p>
+		<p>{{ $message->created_at }}</p>
+	</div>
+	@endforeach	
+</div>
 @endsection
